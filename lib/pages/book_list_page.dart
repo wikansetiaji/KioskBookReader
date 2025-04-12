52c08c386094263dc5adb9b234d5519bd2085dbb
Wikan Setiaji <wikansetiaji@gmail.com>
2025-04-12 18:29:54 +0700

    initial commit

--- lib/pages/book_list_page.dart
+++ lib/pages/book_list_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:kiosk_book_reader/components/book_card.dart';
 import 'package:kiosk_book_reader/models/book.dart';
