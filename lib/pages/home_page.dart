f8e8ef438616d3832bfd760cac770e8ca26befd8
Wikan Setiaji <wikansetiaji@gmail.com>
2025-04-14 15:52:43 +0700

    minor fixes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -58,10 +58,7 @@ class _HomePageState extends State<HomePage> {
           alignment: Alignment.center,
           children: [
             Positioned.fill(
-              child: Image.asset(
-                'assets/bg-texture.png',
-                fit: BoxFit.cover,
-              ),
+              child: Image.asset('assets/bg-texture.png', fit: BoxFit.cover),
             ),
             Column(
               children: [
@@ -132,144 +129,163 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
                 Expanded(
-                  child: Stack(
-                    alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CarouselSlider.builder(
-                        itemCount: books.length,
-                        carouselController: booksCarouselController,
-                        options: CarouselOptions(
-                          aspectRatio: 1.8,
-                          viewportFraction: 0.35,
-                          initialPage: 0,
-                          enableInfiniteScroll: false,
-                          enlargeCenterPage: true,
-                          enlargeFactor: 0.3,
-                          autoPlay: true,
-                          autoPlayInterval: Duration(seconds: 3),
-                          autoPlayAnimationDuration: Duration(
-                            milliseconds: 2000,
-                          ),
-                          autoPlayCurve: Curves.fastOutSlowIn,
-                          scrollDirection: Axis.horizontal,
-                          onPageChanged: (index, reason) {
-                            _selectedIndex = index;
-                          },
-                          onScrolled: (value) {
-                            setState(() {
-                              _carouselOffset = value ?? 0;
-                            });
-                          },
-                        ),
-                        itemBuilder: (context, index, realIndex) {
-                          final book = books[index];
-                          double distanceFromCenter =
-                              ((_carouselOffset % books.length) - index);
-                          double rotationY =
-                              (distanceFromCenter * -1).clamp(-1.0, 1.0) *
-                              0.5; // radians
-
-                          return Transform(
-                            transform:
-                                Matrix4.identity()
-                                  ..setEntry(3, 2, 0.001) // perspective
-                                  ..rotateY(rotationY)
-                                  ..translate(distanceFromCenter * 30),
-                            alignment: Alignment.center,
-                            child: GestureDetector(
-                              onTap: () async {
-                                booksCarouselController.animateToPage(
-                                  index,
-                                  duration: Duration(milliseconds: 300),
-                                  curve: Curves.easeInOut,
-                                );
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CarouselSlider.builder(
+                            itemCount: books.length,
+                            carouselController: booksCarouselController,
+                            options: CarouselOptions(
+                              aspectRatio: 1.8,
+                              viewportFraction: 0.35,
+                              initialPage: 0,
+                              enableInfiniteScroll: false,
+                              enlargeCenterPage: true,
+                              enlargeFactor: 0.3,
+                              autoPlay: true,
+                              autoPlayInterval: Duration(seconds: 3),
+                              autoPlayAnimationDuration: Duration(
+                                milliseconds: 2000,
+                              ),
+                              autoPlayCurve: Curves.fastOutSlowIn,
+                              scrollDirection: Axis.horizontal,
+                              onPageChanged: (index, reason) {
+                                _selectedIndex = index;
                               },
-                              child: Card(
-                                elevation: 3,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: SizedBox(
-                                    width: double.infinity, // Takes full width
-                                    child: Image.asset(
-                                      'assets/${book.id}/cover.jpg',
-                                      fit:
-                                          BoxFit
-                                              .fill, // Changed from fill to cover
-                                      alignment: Alignment.center,
+                              onScrolled: (value) {
+                                setState(() {
+                                  _carouselOffset = value ?? 0;
+                                });
+                              },
+                            ),
+                            itemBuilder: (context, index, realIndex) {
+                              final book = books[index];
+                              double distanceFromCenter =
+                                  ((_carouselOffset % books.length) - index);
+                              double rotationY =
+                                  (distanceFromCenter * -1).clamp(-1.0, 1.0) *
+                                  0.5; // radians
+
+                              return Transform(
+                                transform:
+                                    Matrix4.identity()
+                                      ..setEntry(3, 2, 0.001) // perspective
+                                      ..rotateY(rotationY)
+                                      ..translate(distanceFromCenter * 30),
+                                alignment: Alignment.center,
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    booksCarouselController.animateToPage(
+                                      index,
+                                      duration: Duration(milliseconds: 300),
+                                      curve: Curves.easeInOut,
+                                    );
+                                  },
+                                  child: Card(
+                                    elevation: 3,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: SizedBox(
+                                        width:
+                                            double.infinity, // Takes full width
+                                        child: Image.asset(
+                                          'assets/${book.id}/cover.jpg',
+                                          fit:
+                                              BoxFit
+                                                  .fill, // Changed from fill to cover
+                                          alignment: Alignment.center,
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
+                              );
+                            },
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (_selectedIndex == 0) {
+                                    booksCarouselController.animateToPage(
+                                      books.length - 1,
+                                    );
+                                  } else {
+                                    booksCarouselController.previousPage();
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(16),
+                                  backgroundColor: Color.fromARGB(
+                                    255,
+                                    162,
+                                    29,
+                                    58,
+                                  ),
+                                ),
+                                child: Icon(
+                                  Icons.chevron_left,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      ),
-                      Row(
-                        children: [
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_selectedIndex == 0) {
-                                booksCarouselController.animateToPage(
-                                  books.length - 1,
-                                );
-                              } else {
-                                booksCarouselController.previousPage();
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              shape: CircleBorder(),
-                              padding: EdgeInsets.all(16),
-                              backgroundColor: Color.fromARGB(255, 162, 29, 58),
-                            ),
-                            child: Icon(
-                              Icons.chevron_left,
-                              color: Colors.white,
-                            ),
+                              Expanded(child: Container()),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (_selectedIndex == books.length - 1) {
+                                    booksCarouselController.animateToPage(0);
+                                  } else {
+                                    booksCarouselController.nextPage();
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(16),
+                                  backgroundColor: Color.fromARGB(
+                                    255,
+                                    162,
+                                    29,
+                                    58,
+                                  ),
+                                ),
+                                child: Icon(
+                                  Icons.chevron_right,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                            ],
                           ),
-                          Expanded(child: Container()),
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_selectedIndex == books.length - 1) {
-                                booksCarouselController.animateToPage(0);
-                              } else {
-                                booksCarouselController.nextPage();
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              shape: CircleBorder(),
-                              padding: EdgeInsets.all(16),
-                              backgroundColor: Color.fromARGB(255, 162, 29, 58),
-                            ),
-                            child: Icon(
-                              Icons.chevron_right,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: 20),
                         ],
+                      ),
+                      SizedBox(height: 25),
+                      Text(
+                        books[_selectedIndex].author.toUpperCase(),
+                        style: TextStyle(
+                          fontFamily: 'Archivo',
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 119, 24, 45),
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        books[_selectedIndex].title.toUpperCase(),
+                        style: TextStyle(
+                          fontFamily: 'Archivo',
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 85, 85, 85),
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
                     ],
                   ),
-                ),
-                Text(
-                  books[_selectedIndex].author.toUpperCase(),
-                  style: TextStyle(
-                    fontFamily: 'Archivo',
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 119, 24, 45),
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  books[_selectedIndex].title.toUpperCase(),
-                  style: TextStyle(
-                    fontFamily: 'Archivo',
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 85, 85, 85),
-                  ),
-                  textAlign: TextAlign.center,
                 ),
                 SizedBox(height: 30),
                 ElevatedButton(
@@ -293,11 +309,11 @@ class _HomePageState extends State<HomePage> {
                       fontFamily: 'Archivo',
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                      fontSize: 24,
+                      fontSize: 18,
                     ),
                   ),
                 ),
-                SizedBox(height: 15),
+                SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
@@ -315,7 +331,7 @@ class _HomePageState extends State<HomePage> {
                       fontFamily: 'Archivo',
                       fontWeight: FontWeight.bold,
                       color: Color.fromARGB(255, 162, 29, 58),
-                      fontSize: 24,
+                      fontSize: 18,
                     ),
                   ),
                 ),
